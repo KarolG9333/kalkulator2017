6f9409273e323aa7e750ccdc5c8a4a21a7433470
Karol Góra <41211@student.ue.poznan.pl>
2017-04-11 19:49:53 +0200

    rozwiazalem konflikt

--- kalkulatory.py
+++ kalkulatory.py
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-def dodawanie():
-	a = int(input('Podaj pierwszą liczbe'))
-	b = int(input('Podaj pierwszą liczbe'))
-	print(a+b)
-
 def get_help():
 	print("To jest prosty program kalkulaktora")
-	
-get_help()
-dodawanie()
-=======
+
 def dodawanie(a, b):
 	wynik = a + b 
 	return wynik
-
-	
+get_help()
 zm1 = int(input())	
 zm2 = int(input())
 print(dodawanie(zm1, zm2))
->>>>>>> zmiana_funkcji_dodawania
+
+
+	
+
